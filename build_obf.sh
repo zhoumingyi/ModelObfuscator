04047f93cf81e8634046c7f1ca622ae978cdf8e8
Mingyi Zhou <zhoumingyigege@gmail.com>
2023-07-20 04:22:25 -0700

    Update build_obf.sh

--- build_obf.sh
+++ build_obf.sh
@@ -1,7 +1,10 @@
 python obfuscation.py --model_name=fruit --extra_layer=30 --shortcut=30
-flatc -t schema.fbs -- obf_model.tflite
-python modify_tflite.py
-flatc -b ./schema.fbs obf_model.json
+# ----------------------------------------------
+# uncomment the following lines if you want to test the uint8 model (but we currently only support limited ops for uint8 model)
+# flatc -t schema.fbs -- obf_model.tflite
+# python modify_tflite.py
+# flatc -b ./schema.fbs obf_model.json
+# ----------------------------------------------
 pip uninstall -y tensorflow
 pip install /tmp/tensorflow_pkg/tensorflow-2.9.1-cp38-cp38-linux_x86_64.whl
 python test_model.py --model_name=fruit
