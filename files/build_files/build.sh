97003daa3492e7c705bd380448f6e414f57f6c54
rdass <rdass@sba-research.org>
2024-04-03 13:39:08 +0200

    updating path in the build.sh file

--- files/build_files/build.sh
+++ files/build_files/build.sh
@@ -1,3 +1,13 @@
-bazel build --jobs=14 //tensorflow/tools/pip_package:build_pip_package
+#Original_code
+# bazel build --jobs=14 //tensorflow/tools/pip_package:build_pip_package
 # sleep 5
-./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+# ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+
+#Modified code
+
+bazel build --jobs=14 //tensorflow/tools/pip_package:build_pip_package
+echo "Hello, generating a package unpacking successfull "
+echo $PWD
+mkdir -p /tmp/tensorflow_pkg && ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+echo "Hello, package creation is successfull "
+
